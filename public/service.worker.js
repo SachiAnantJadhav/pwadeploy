cb30eeb3406af41b96d671da50ed11b45733def4
SachiAnantJadhav <sachijadhav2125@gmail.com>
2025-04-21 10:38:15 +0530

    first commit

--- public/service.worker.js
+++ public/service.worker.js
@@ -3,7 +3,7 @@ self.addEventListener('install', (e) => {
     e.waitUntil(
       caches.open('ecom-cache').then((cache) => {
         return cache.addAll([
-          '/',
+          '/pwadeploy/',
           '/pwadeploy/index.html',
           '/pwadeploy/manifest.json',
           '/pwadeploy/logo192.png',
